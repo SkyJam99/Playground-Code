a6e098468d3ea6095c0b706b71534b3321fc8ab2
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-02-20 15:08:02 -0400

    Finished edge cases for q2

--- AofAAssignment3/q2.py
+++ AofAAssignment3/q2.py
@@ -1,7 +1,18 @@
 def findMissing(A, front, back):
     mid = (int) (front + back) // 2
-    print("Front:" + front + " Mid:" + A[mid] + " Back:" + A[back])
-    #Base Case
+    print("Front:" + str(A[front]) + " Mid:" + str(A[mid]) + " Back:" + str(A[back]))
+    
+    #End Case
+    if front >= back:
+        print("Back!")
+        return A[back] + 1
+
+    #Front Case
+    if(A[front] != (front+1)):
+        print("Front!")
+        return A[front]-1
+    
+    #Mid Case
     if (A[mid]+1) != (A[mid+1]):
         return A[mid]+1
     
@@ -13,6 +24,6 @@ def findMissing(A, front, back):
         return findMissing(A, front, mid-1)
     
 
-A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 print(findMissing(A, 0, len(A)-1))
